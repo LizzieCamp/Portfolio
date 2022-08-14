52b5ab52d546b5767dbf649e85263f515fbad380
LizzieCamp <elizabeth.camp14@gmail.com>
2022-08-14 20:26:52 +0100

    feat: add experience object and map

--- elizabeth-portfolio/src/pages/Experience.jsx
+++ elizabeth-portfolio/src/pages/Experience.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import "../Styles/styles";
 import Back from "../components/BackButton";
+import Card from "../components/Card";
+
+const experience = [{
+  title: "Elsevier",
+  subTitle: "Software Engineer III",
+  content: "i am se3",
+  subTitle1: "Software En gineer II",
+  content1: "i am se2",
+  subTitle2: "Technology Associate",
+  content2: "i am ta"
+}];
+
+
 
 const Experience = () => {
   return (
     <div className="margins">
       <Back />
+      <h1> Experience </h1>
       <div className="padding">
-        <h1> Experience </h1>
+        {experience.map((job, subTitle) => (
+          <Card key={subTitle} index={subTitle} {...job} />
+        ))}
       </div>
     </div>
   );
