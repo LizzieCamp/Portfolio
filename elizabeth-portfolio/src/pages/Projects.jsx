682e1ea77bf96ce26a0dd23073a92008bd381d64
LizzieCamp <elizabeth.camp14@gmail.com>
2022-08-14 20:27:07 +0100

    feat: add projects array and map through

--- elizabeth-portfolio/src/pages/Projects.jsx
+++ elizabeth-portfolio/src/pages/Projects.jsx
@@ -2,17 +2,29 @@ import React from "react";
 import "../Styles/styles";
 import Back from "../components/BackButton";
 import Card from "../components/Card";
+
+const projects = [{
+  title: "title of cool project",
+  content: "this cool project be cool"
+},
+{
+  title: "wow another one",
+  content: "wow again"
+},
+{
+  title: "ok too many",
+  content: "or is it too many"
+}];
+
 const Projects = () => {
   return (
     <div className="margins">
       <Back />
-        <h1> Projects </h1>
+      <h1> Projects </h1>
       <div className="content">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-
+        {projects.map((project, title) => (
+          <Card key={title} index={title} {...project} />
+        ))}
       </div>
     </div>
   );
