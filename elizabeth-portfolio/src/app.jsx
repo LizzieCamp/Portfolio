0e79cf76ce08ac2f3cb87f8580cf9e7c0f188de8
LizzieCamp <elizabeth.camp14@gmail.com>
2022-09-03 09:20:03 +0100

    refactor: remove routes bit for now

--- elizabeth-portfolio/src/app.jsx
+++ elizabeth-portfolio/src/app.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Styles/styles";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Start from "./pages/Start";
@@ -7,19 +7,40 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
+import NewHome from "./pages/NewHome";
+import SideBar from "./components/SideBar";
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <Router>
-      <Routes>
-        <Route index element={<Start />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/experience" element={<Experience />} />
-      </Routes>
-    </Router>
+    <div>
+      <SideBar isOpen={isOpen} toggle={toggle} />
+      <NewHome isOpen={isOpen} toggle={toggle} />
+      <Experience />
+    </div>
+    // <Router>
+    //   <Routes>
+    //     <Route
+    //       index
+    //       element={
+    //         <>
+    //           <SideBar isOpen={isOpen} toggle={toggle} />
+    //           <NewHome isOpen={isOpen} toggle={toggle} />{" "}
+    //         </>
+    //       }
+    //     />
+    //     <Route path="/home" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //     <Route path="/projects" element={<Projects />} />
+    //     <Route path="/experience" element={<Experience />} />
+    //   </Routes>
+    // </Router>
   );
 };
 
