00b4be63d2441ee2514387e5503d74e287e42e6f
LizzieCamp <elizabeth.camp14@gmail.com>
2022-10-01 12:29:20 +0100

    feat: about section

--- elizabeth-portfolio/src/pages/About.jsx
+++ elizabeth-portfolio/src/pages/About.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "../Styles/styles";
+import Card from "../components/Card";
+
+const aboutInfo = [{
+  title: "Currently working in Elsevier",
+  content: "Like the front end ",
+}]
 
 const About = () => {
   return (
-    <div className="about margins">
+    <div className="page margins">
       <h1> About </h1>
       <div className="content aboutPage">
-      <p className="aboutText">
+      {/* <p className="aboutText">
       text here woop text here woop text here woop text here woop text here woop text here woop text here woop text here woop text here woop 
-      </p>
+      </p> */}
+      {aboutInfo.map((about, title) => (
+      <Card class="About" key={title} index={title} {...about}/>
+      ))}
       <img
           className="aboutImage"
           src="AvatarMaker.png"
