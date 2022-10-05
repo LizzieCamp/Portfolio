b0d6b44100f08e48848b24548e70fbf93d1887ff
LizzieCamp <elizabeth.camp14@gmail.com>
2022-10-05 08:22:31 +0100

    refactor: capitalise everything

--- elizabeth-portfolio/src/Styles/styles.js
+++ elizabeth-portfolio/src/Styles/styles.js
@@ -1,12 +1,12 @@
-import "./mainStyles.scss";
-import "./Bubble.scss";
+import "./Main.scss";
 import "./Card.scss";
-import "./NewHome.scss";
+import "./Home.scss";
 import "./Experience.scss";
 import "./Projects.scss";
 import "./About.scss";
 import "./Contact.scss";
-import "./formInput.scss";
+import "./FormInput.scss";
 import "./Form.scss";
 import "./Button.scss";
 import "./Success.scss";
+import "./Footer.scss";
