de33816a76c592e6532357ac30690e6817933c2e
LizzieCamp <elizabeth.camp14@gmail.com>
2022-10-05 08:20:00 +0100

    refactor: rename NewHome to Home - remove old Home file

--- elizabeth-portfolio/src/pages/Home.jsx
+++ elizabeth-portfolio/src/pages/Home.jsx
@@ -1,38 +1,21 @@
 import React from "react";
 import "../Styles/styles";
-import Bubble from "../components/Bubble";
-import PDF from "../CV.pdf";
 
 const Home = () => {
   return (
-    <div className="margins">
-      <div className="padding home">
-        <img className="meImage" src="meCircle.png" alt="Picture of girl" />
-        <a href="https://github.com/LizzieCamp">
-          <img className="github" src="github.png" alt="Github logo" />
-        </a>
-        <a href="https://www.linkedin.com/in/elizabeth-camp-0366b8165/">
-          <img className="linkedin" src="linkedin.png" alt="Linkedin logo" />
-        </a>
-        <Bubble
-          route="/experience"
-          classes="circle experience"
-          message="Experience"
-        ></Bubble>
-        <Bubble route="/about" classes="circle about" message="About"></Bubble>
-        <Bubble
-          target="_blank"
-          rel="noreferrer"
-          classes="circle cv"
-          message="cv"
-          link={PDF}
-        ></Bubble>
-        <Bubble classes="circle contact" message="Contact"></Bubble>
-        <Bubble
-          route="/projects"
-          classes="circle projects"
-          message="Projects"
-        ></Bubble>
+    <div>
+      <div className="padding home ">
+        <h1 className="animated animatedFadeInUp fadeInUp introText">
+          Hello.
+          <br />
+          Im Elizabeth.
+          <br />A frontend Software Engineer.
+        </h1>
+        <img
+          className="animated animatedFadeInUp delayed fadeInUp meImage"
+          src="AvatarMaker.png"
+          alt="elizabeth camp"
+        />
       </div>
     </div>
   );
