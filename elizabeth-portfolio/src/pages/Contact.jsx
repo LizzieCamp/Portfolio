7934d6eae3c778c81b59b3d4bea4c93caa0af582
LizzieCamp <elizabeth.camp14@gmail.com>
2022-10-05 08:21:28 +0100

    feat: add animate on scroll to contact

--- elizabeth-portfolio/src/pages/Contact.jsx
+++ elizabeth-portfolio/src/pages/Contact.jsx
@@ -1,19 +1,30 @@
-import React, { useRef } from "react";
+import React from "react";
 import "../Styles/styles";
 import Form from "../components/Form";
-import emailjs from "@emailjs/browser";
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const Contact = () => {
-  const resolution = window.innerWidth;
-
-  const isDesktop = resolution >= 1024;
-
   return (
     <div className="margins">
       <h1> Contact </h1>
       <div className="content">
-        <Form />
-        {isDesktop ? <Form /> : undefined}
+        <AnimationOnScroll
+          animateIn="animate__animated animate__slideInLeft animate__slower"
+          animateOnce="true"
+        >
+          <Form />
+        </AnimationOnScroll>
+          <AnimationOnScroll
+            animateIn="animate__animated animate__slideInRight animate__slower"
+            animateOnce="true"
+          >
+            <img
+              className="aboutImage"
+              src="AvatarMaker.png"
+              alt="elizabeth camp"
+            />
+          </AnimationOnScroll>
       </div>
     </div>
   );
