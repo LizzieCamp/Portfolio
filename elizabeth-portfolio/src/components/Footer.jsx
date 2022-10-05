4c8b101f24f952902dfa0ffedbf2d7ced89e71e9
LizzieCamp <elizabeth.camp14@gmail.com>
2022-10-05 08:22:59 +0100

    feat: create footer component

---
+++ elizabeth-portfolio/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Styles/styles";
+
+export default () => {
+  return (
+    <div className="footer">
+      <a href="https://www.linkedin.com/in/elizabeth-camp-0366b8165/">
+        <img className="logo" src="linkedin.png" alt="elizabeth camp" />
+      </a>
+      <p> Copyright message</p>
+      <a href="https://github.com/LizzieCamp">
+        <img className="logo" src="github.png" alt="elizabeth camp" />
+      </a>
+    </div>
+  );
+};
