a4b6b37d6d76223803b5278ea6360c6df2e50453
LizzieCamp <elizabeth.camp14@gmail.com>
2022-10-07 20:47:09 +0100

    refactor: remove unused imports

--- elizabeth-portfolio/src/Styles/styles.js
+++ elizabeth-portfolio/src/Styles/styles.js
@@ -1,12 +1,11 @@
 import "./Main.scss";
 import "./Card.scss";
 import "./Home.scss";
-import "./Experience.scss";
-import "./Projects.scss";
 import "./About.scss";
-import "./Contact.scss";
 import "./FormInput.scss";
 import "./Form.scss";
 import "./Button.scss";
 import "./Success.scss";
 import "./Footer.scss";
+import "./SideBar.scss"
+import "./Header.scss";
