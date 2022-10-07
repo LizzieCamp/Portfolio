b3dc79694f9a343c933b89072cf8831290602f5f
LizzieCamp <elizabeth.camp14@gmail.com>
2022-10-07 20:50:49 +0100

    refactor: map through navbar

--- elizabeth-portfolio/src/components/Header.jsx
+++ elizabeth-portfolio/src/components/Header.jsx
@@ -2,33 +2,56 @@ import React from "react";
 import "../Styles/styles";
 import { FaBars } from "react-icons/fa";
 import PDF from "../CV.pdf";
+import ListLink from "./ListLink";
+import ImageLink from "./ImageLink";
 
-export default ({ toggle }) => {
+export default ({ toggle }, props) => {
+  const navBarItems = [
+    {
+      href: "#About",
+      linkClass: "navLink",
+      label: "About",
+    },
+    {
+      href: "#Projects",
+      linkClass: "navLink",
+      label: "Projects",
+    },
+    {
+      href: "#Experience",
+      linkClass: "navLink",
+      label: "Experience",
+    },
+    {
+      href: "#Contact",
+      linkClass: "navLink",
+      label: "Contact",
+    },
+    {
+      href: PDF,
+      linkClass: "navLink",
+      label: "CV",
+      target: "_blank",
+      rel: "noreferrer",
+    },
+  ];
+
   return (
     <div className="nav">
       <div className="navContainer">
-        <div className="navLogo">LOGO</div>
+        <ImageLink
+          href="#home"
+          src="AvatarMaker.png"
+          altText="Elizabeth Camp Portfolio"
+          class="navLogo"
+        />
         <div className="burgerMenu" onClick={toggle}>
           <FaBars />
         </div>
         <ul className="navMenu">
-          <li className="navItem">
-            <a className="navLink">About</a>
-          </li>
-          <li className="navItem">
-            <a className="navLink">Experience</a>
-          </li>
-          <li className="navItem">
-            <a className="navLink">Projects</a>
-          </li>
-          <li className="navItem">
-            <a className="navLink">Contact</a>
-          </li>
-          <li className="navItem">
-            <a target="_blank" rel="noreferrer" href={PDF} className="navLink">
-              CV
-            </a>
-          </li>
+          {navBarItems.map((item, label) => (
+            <ListLink key={label} index={label} {...item} />
+          ))}
         </ul>
       </div>
     </div>
--- elizabeth-portfolio/src/components/SideBar.jsx
+++ elizabeth-portfolio/src/components/SideBar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../Styles/styles";
 import { FaTimes } from "react-icons/fa";
+import PDF from "../CV.pdf";
+import ListLink from "./ListLink";
 
 const SideBar = ({ isOpen, toggle }) => {
   let customStyle = {};
@@ -11,32 +13,46 @@ const SideBar = ({ isOpen, toggle }) => {
     };
   }
 
+  const navBarItems = [
+    {
+      itemClass: "sideBarLink",
+      href: "#About",
+      label: "About",
+    },
+    {
+      itemClass: "sideBarLink",
+      href: "#Projects",
+      label: "Projects",
+    },
+    {
+      itemClass: "sideBarLink",
+      href: "#Experience",
+      label: "Experience",
+    },
+    {
+      itemClass: "sideBarLink",
+      href: "#Contact",
+      label: "Contact",
+    },
+    {
+      itemClass: "sideBarLink",
+      href: PDF,
+      label: "CV",
+      target: "_blank",
+      rel: "noreferrer",
+    },
+  ];
+
   return (
-    <div
-      className="sideBar"
-      onClick={toggle}
-      style={customStyle}
-    >
+    <div className="sideBar" onClick={toggle} style={customStyle}>
       <div className="closeIcon" onClick={toggle}>
         <FaTimes />
       </div>
       <div className="sideBarWrapper">
         <ul className="sideBarMenu">
-          <li className="sideBarLink">
-            <a>About</a>
-          </li>
-          <li className="sideBarLink">
-            <a>Experience</a>
-          </li>
-          <li className="sideBarLink">
-            <a>Projects</a>
-          </li>
-          <li className="sideBarLink">
-            <a>Contact</a>
-          </li>
-          <li className="sideBarLink">
-            <a>CV</a>
-          </li>
+          {navBarItems.map((item, label) => (
+            <ListLink key={label} index={label} {...item} />
+          ))}
         </ul>
       </div>
     </div>
