e6282b055416210d34b31969bbb98148150ad52d
LizzieCamp <elizabeth.camp14@gmail.com>
2022-10-20 06:56:50 +0100

    feat: add altText

--- elizabeth-portfolio/src/components/FormSuccess.jsx
+++ elizabeth-portfolio/src/components/FormSuccess.jsx
@@ -5,7 +5,7 @@ import Image from "./Image";
 export default () => {
   return (
     <div className="form">
-      <Image class="checkMark" src="check-mark.png" altText="Elizabeth Camp" />
+      <Image class="checkMark" src="check-mark.png" altText="Check mark to confirm the page was submitted successfully" />
       <p> Thank You! </p>
       <p> The form was submitted successfully. </p>
     </div>
