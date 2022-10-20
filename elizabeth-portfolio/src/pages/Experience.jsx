28f5be9e677a783af51252b608c73aa2828df227
LizzieCamp <elizabeth.camp14@gmail.com>
2022-10-20 06:56:42 +0100

    feat: add content

--- elizabeth-portfolio/src/pages/Experience.jsx
+++ elizabeth-portfolio/src/pages/Experience.jsx
@@ -5,32 +5,22 @@ import Card from "../components/Card";
 const experience = [
   {
     title: "Elsevier",
-    subTitle: "Software Engineer III",
-    content: "i am se3",
-    subtitleTwo: "Software Engineer II",
-    additionalContent: "i am se2",
-    subtitleThree: "Technology Associate",
+    subTitle: "Software Engineer III · May 2022 - Present",
+    content: "Working on the 'Next Gen' project, rebuilding the elsevier.com site. This involves working to design specifications, building out React components and content models in GraphQl.",
+    subtitleTwo: "Software Engineer II · Sept 2020 - May 2022",
+    additionalContent: "As an SE2 I worked more on improving; the Accessibility rating of Elsevier.com, the speed and performance metrics and the teams agile processes.",
+    subtitleThree: "Technology Associate · Sept 2019 - Sept 2020",
     extraContent:
-      "i am tai am tai am ti am tai am tai am tai am tai am tai am tai am t i am tai am tai am tai am tai am tai am tai am tai am tai am tai am taai am ta",
+      "I joined Elsevier as part of their graduate scheme. I was placed in the Elsevier.com team. Where at the time my role was to maintain the Elsevier.com website and add small changes and improvements.",
   },
   {
-    title: "Elsevier",
-    subTitle: "Software Engineer III",
-    content: "i am se3",
-    subtitleTwo: "Software Engineer II",
-    additionalContent: "i am se2",
-    subtitleThree: "Technology Associate",
-    extraContent: "i am ta",
+    title: "Univeristy of Reading",
+    subTitle: "Bsc Computer Science 2016-2019",
+    subtitleTwo: "Grade: 2.1",
+    subtitleThree: "Modules studied include:",
+    extraContent: "Codes and code breaking · Algorithms and operating systems · Software Quality and Testing ·",
   },
-  {
-    title: "Elsevier",
-    subTitle: "Software Engineer III",
-    content: "i am se3",
-    subtitleTwo: "Software Engineer II",
-    additionalContent: "i am se2",
-    subtitleThree: "Technology Associate",
-    extraContent: "i am ta",
-  },
+
 ];
 
 const Experience = () => {
--- elizabeth-portfolio/src/pages/Projects.jsx
+++ elizabeth-portfolio/src/pages/Projects.jsx
@@ -2,22 +2,47 @@ import React from "react";
 import "../Styles/styles";
 import Card from "../components/Card";
 
-
 const projects = [{
-  title: "Title of cool project",
-  content: "This cool project be cool",
-  image: "AvatarMaker.png",
-  imageLink: "www.google.com",
-  imgSrc: "AvatarMaker.png",
-  linkAltText: "some alt text here"
+  title: "Movies API: 'to watch list'",
+  content: "This project uses a Movie API (OMDb API) which allows users to create their own 'watch list'. A to do list style app where users add films to their 'to watch' list and can check them off - moving the film to their 'watched' list.",
+  image: "placeholder-image.png",
+  // altText:
+  additionalContent: "React · Movie API · ",
+  imageLink: "https://github.com/LizzieCamp",
+  ariaLabel: "Go to the movie api projects github repository",
+  imgSrc: "github.png",
+  linkAltText: "Github"
+
 },
 {
-  title: "wow another one",
-  content: "wow again"
+  title: "Fight Club",
+  content: "A CLI based fight simulator.",
+  image: "placeholder-image.png",
+  additionalContent: "Typescript",
+  imageLink: "https://github.com/LizzieCamp",
+  ariaLabel: "Go to the fight club projects github repository",
+  imgSrc: "github.png",
+  linkAltText: "Github"
 },
 {
-  title: "ok too many",
-  content: "or is it too many"
+  title: "Blog site",
+  content: "A blog site where users can create, edit and delete their own blog posts.",
+  additionalContent: "React · Sass/Scss · MongoDB",
+  image: "placeholder-image.png",
+  imageLink: "https://github.com/LizzieCamp",
+  ariaLabel: "Go to the blog site projects github repository",
+  imgSrc: "github.png",
+  linkAltText: "Github"
+},
+{
+  title: "Weather Reporter",
+  content: "A simple weather reporting app which can: ",
+  additionalContent: "React · GraphQL · Weather API",
+  image: "placeholder-image.png",
+  imageLink: "https://github.com/LizzieCamp",
+  ariaLabel: "Go to the weather reporter projects github repository",
+  imgSrc: "github.png",
+  linkAltText: "Github"
 }];
 
 const Projects = () => {
@@ -26,7 +51,7 @@ const Projects = () => {
       <h1> Projects </h1>
       <div className="content">
         {projects.map((project, title) => (
-          <Card  key={title} index={title} {...project} />
+          <Card  class="projects" key={title} index={title} {...project} />
         ))}
       </div>
     </div>
