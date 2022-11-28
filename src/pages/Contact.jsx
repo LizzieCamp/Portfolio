4fd5cfaff4d83188cfece1fd41d868a0db22593e
LizzieCamp <elizabeth.camp14@gmail.com>
2022-11-28 21:31:01 +0000

    feat: more changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,20 +3,23 @@ import "../Styles/styles";
 import Form from "../components/Form";
 import "animate.css/animate.min.css";
 import { AnimationOnScroll } from "react-animation-on-scroll";
-import Image from "../components/Image";
 import Card from "../components/Card";
 
-const contact = [{
-  content: "Got a question, or just want to say hi?",
-  additionalContent: "Feel free to get in touch with me by using this form and I will get back to you as soon as possible.",
-  imgSrc: "https://images.unsplash.com/photo-1636193535246-a07cd0aa6fcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-}]
+const contact = [
+  {
+    content: "Got a question, or just want to say hi?",
+    additionalContent:
+      "Feel free to get in touch with me by using this form and I will get back to you as soon as possible.",
+    imgSrc:
+      "https://images.unsplash.com/photo-1636193535246-a07cd0aa6fcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+];
 
 const Contact = () => {
   return (
-    <div id="Contact"className="page margins">
+    <div id="Contact" className="page margins">
       <h1>Send me a message!</h1>
-      
+
       <div className="content">
         <AnimationOnScroll
           animateIn="animate__animated animate__slideInLeft animate__slower"
@@ -29,15 +32,13 @@ const Contact = () => {
           animateOnce="true"
         >
           {contact.map((info, subTitle) => (
-          <Card class="contactImage" key={subTitle} index={subTitle} {...info} />
-        ))}
-          {/* <Image
-            class="aboutImage"
-            src="https://images.unsplash.com/photo-1636193535246-a07cd0aa6fcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-            altText="Elizabeth Camp"
-          />
-          <h4>Got a question, or just want to say hi?</h4>
-      <h4>Feel free to get in touch with me by using the form below and I will get back to you as soon as possible.</h4> */}
+            <Card
+              class="contactImage"
+              key={subTitle}
+              index={subTitle}
+              {...info}
+            />
+          ))}
         </AnimationOnScroll>
       </div>
     </div>
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -6,13 +6,13 @@ const experience = [
   {
     title: "Elsevier",
     subTitle: "Software Engineer III · May 2022 - Present",
-    content: "Working on the 'Next Gen' project, rebuilding the elsevier.com site. This involves working to design specifications, building out React components and content models in GraphQl.",
+    content: "Working on rebuilding the elsevier.com site. This involves working to design specifications, building out React components and content models in GraphQl.",
     subtitleTwo: "Software Engineer II · Sept 2020 - May 2022",
     additionalContent:
-      "As an SE2 I worked more on improving; the Accessibility rating of Elsevier.com, the speed and performance metrics and the teams agile processes.",
+      "Worked on improving the Accessibility rating of Elsevier.com, the speed and performance metrics and the teams agile processes.",
     subtitleThree: "Technology Associate · Sept 2019 - Sept 2020",
     extraContent:
-      "I joined Elsevier as part of their graduate scheme. I was placed in the Elsevier.com team. Where at the time my role was to maintain the Elsevier.com website and add small changes and improvements.",
+      "My role upon joining as a graduate was to maintain the Elsevier.com website and add small changes and improvements.",
   },
   {
     title: "Univeristy of Reading",
