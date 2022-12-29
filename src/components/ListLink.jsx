6d020315f97c7777e8c22f644109035d79562e4c
LizzieCamp <elizabeth.camp14@gmail.com>
2022-12-29 11:02:34 +0000

    feat: changes for active tab

--- src/components/ListLink.jsx
+++ src/components/ListLink.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "../Styles/styles";
+import { Link } from "react-scroll";
 
 export default (props) => {
   return (
     <li className={props.itemClass}>
-      <a aria-label={props.ariaLabel} target={props.target} rel={props.rel }href={props.href} className={props.linkClass}>
-        {props.label}
-      </a>
+      <Link
+        activeClass="active"
+        className={props.linkClass}
+        to={props.label}
+        spy={true}
+        smooth={true}
+        duration={500}
+      >
+        <p>{props.label}</p>
+      </Link>
     </li>
   );
 };
